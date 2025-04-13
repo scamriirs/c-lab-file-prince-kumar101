76b74ed77c43bf608c224c0eda4730755ed4c2b2
Prince Kumar <princekumarstudy@gmail.com>
2025-04-13 13:59:02 +0530

    Create Program22.cs

---
+++ Program22.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace StudentDatabaseApp
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = "Data Source=localhost;Initial Catalog=StudentDB;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFetch_Click(object sender, EventArgs e)
+        {
+            string regNo = txtRegNo.Text;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Students WHERE RegistrationNo = @regNo";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@regNo", regNo);
+
+                try
+                {
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        txtName.Text = reader["Name"].ToString();
+                        txtAge.Text = reader["Age"].ToString();
+                        txtClass.Text = reader["Class"].ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No record found!");
+                    }
+
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
